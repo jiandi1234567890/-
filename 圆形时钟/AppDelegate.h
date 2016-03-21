a8bce818563901e5f4badb62472a2fb4a5ff3f51
jiandi1234567890 <550392461@qq,com>
2016-03-21 09:10:33 +0800

    Initial Commit

---
+++ 圆形时钟/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  圆形时钟
+//
+//  Created by 张海禄 on 16/3/21.
+//  Copyright © 2016年 张海禄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
